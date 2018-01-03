83ee3acbebe2fcbd4c793b4e033384430da8b74d
Dmytro Andrusiv <dmytro.andrusiv@gmail.com>
2018-01-03 11:08:38 +0200

    corrected name of variables

--- src/main/java/com/warmcity/citygrants/models/Evaluation.java
+++ src/main/java/com/warmcity/citygrants/models/Evaluation.java
@@ -12,8 +12,8 @@ public class Evaluation implements Serializable {
 
   private static final long serialVersionUID = 8391566704550860062L;
   
-  private String JuryMemberId;
-  private String JuryMemberName;
+  private String juryMemberId;
+  private String juryMemberName;
   private int evaluation;
 
 }

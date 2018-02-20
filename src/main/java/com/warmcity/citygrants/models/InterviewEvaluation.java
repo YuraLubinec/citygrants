7a00a27999524219d94ab647f587e9285beb5bae
Dimitrij512 <dmytro.andrusiv@gmail.com>
2018-02-21 00:07:18 +0200

    added model interview evaluation

--- src/main/java/com/warmcity/citygrants/models/InterviewEvaluation.java
+++ src/main/java/com/warmcity/citygrants/models/InterviewEvaluation.java
@@ -12,8 +12,8 @@ public class InterviewEvaluation implements Serializable {
 
   private static final long serialVersionUID = 5667820033147334236L;
   
-  private String JuryMemberId;
-  private String JuryMemberName;
+  private String juryMemberId;
+  private String juryMemberName;
   private int evaluation;
   
 }
--- src/main/java/com/warmcity/citygrants/models/Project.java
+++ src/main/java/com/warmcity/citygrants/models/Project.java
@@ -35,6 +35,10 @@ public class Project implements Serializable {
     return evaluations == null ? new ArrayList<Evaluation>(): this.evaluations;
   }
 
+  public List <Comment> getComments(){
+    return this.comments == null ? new ArrayList<Comment>(): this.comments;
+  }
+
   public int getTotalEvalFirstStage() {
     this.totalEvalFirstStage = 0;
     getEvaluations().forEach(evaluation -> {
@@ -48,5 +52,23 @@ public class Project implements Serializable {
     });
 
     return this.totalEvalFirstStage;
+  }
+
+  @Override
+  public String toString() {
+    return "Project{" +
+            "uploadingService=" + uploadingService +
+            ", id='" + id + '\'' +
+            ", description=" + description +
+            ", budget=" + budget +
+            ", evaluations=" + evaluations +
+            ", interviewEvaluations=" + interviewEvaluations +
+            ", comments=" + comments +
+            ", filesInfo=" + filesInfo +
+            ", confirmed=" + confirmed +
+            ", approvedToSecondStage=" + approvedToSecondStage +
+            ", totalEvalFirstStage=" + totalEvalFirstStage +
+            ", totalEvalSecondStage=" + totalEvalSecondStage +
+            '}';
   }
 }
